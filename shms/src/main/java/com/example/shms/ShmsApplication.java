0c8882dbe40fa2b1828fa15e966dfa11340b7316
Tayyab <tayyab.boson@gmail.com>
2025-10-26 15:35:03 +0100

    spring initializer created for shms

---
+++ shms/src/main/java/com/example/shms/ShmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.shms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShmsApplication.class, args);
+	}
+
+}
